5649f65ed9f4a890a5aebf208bc48c1271220c57
Pranav Gonepalli <sgonepalli@wm.edu>
2022-11-19 19:43:27 -0500

    Finished AMazeActivity
    Added full functionality for the buttons and SeekBar. When Explore or Revisit is clicked, it changes to GeneratingActivity, with the skill level, builder, and rooms.

--- app/src/main/java/edu/wm/cs/cs301/pranavgonepalli/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/pranavgonepalli/AMazeActivity.java
@@ -5,21 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.view.View;
-import android.widget.Toast;
-import android.widget.AdapterView;
+import android.content.Intent;
+import android.widget.TextView;
 
 import java.sql.Array;
 
 public class AMazeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     String[] algorithms = {"DFS", "Prim", "Boruvka"};
     String[] rooms = {"Yes", "No"};
-
+    SeekBar skill_level;
+    Spinner generator_spinner;
+    Spinner rooms_spinner;
     /**
      * Create the Spinner that contains the generating algorithms, and ArrayAdapter.
      * The default generator is DFS.
      * Also, creates a Spinner for a yes or no choice for rooms, with the default being yes.
+     * Also changes the TextView containing the maze difficulty based on the change of
+     * the SeekBar.
      * @param savedInstanceState
      */
     @Override
@@ -27,17 +32,56 @@ public class AMazeActivity extends AppCompatActivity implements AdapterView.OnIt
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amaze);
 
-        Spinner generator_spinner = findViewById(R.id.maze_generator);
+        generator_spinner = (Spinner) findViewById(R.id.maze_generator);
         generator_spinner.setOnItemSelectedListener(this);
         ArrayAdapter generator = new ArrayAdapter(this, android.R.layout.simple_spinner_item, algorithms);
         generator.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         generator_spinner.setAdapter(generator);
 
-        Spinner rooms_spinner = findViewById(R.id.maze_rooms);
+        rooms_spinner = (Spinner) findViewById(R.id.maze_rooms);
         rooms_spinner.setOnItemSelectedListener(this);
         ArrayAdapter room = new ArrayAdapter(this, android.R.layout.simple_spinner_item, rooms);
         room.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         rooms_spinner.setAdapter(room);
+
+        skill_level = (SeekBar) findViewById(R.id.maze_size);
+        skill_level.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            /**
+             * When the user changes the progress of the SeekBar, it updates the
+             * skill  level shown in the skill level TextView.
+             * @param seekBar
+             * @param i
+             * @param b
+             */
+             @Override
+             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                 TextView maze_level = findViewById(R.id.maze_size_level);
+                 maze_level.setText("" + i);
+             }
+             @Override
+             public void onStartTrackingTouch(SeekBar seekBar) {
+
+             }
+             @Override
+             public void onStopTrackingTouch(SeekBar seekBar) {
+
+             }
+        });
+    }
+
+    /**
+     * Switch from title state to generating state.
+     * @param v
+     */
+    public void switchToGenerating(View v){
+        int skill = skill_level.getProgress();
+        String builder = generator_spinner.getSelectedItem().toString();
+        String rooms_choice = rooms_spinner.getSelectedItem().toString();
+        Intent intent = new Intent(this, GeneratingActivity.class);
+        intent.putExtra("skill", skill);
+        intent.putExtra("builder", builder);
+        intent.putExtra("rooms", rooms_choice);
+        startActivity(intent);
     }
 
     @Override
